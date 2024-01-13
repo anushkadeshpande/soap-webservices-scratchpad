44363420f69962d7f0335bd4e8ca1f63ef40b164
anushkadeshpande <anushkaa5000@gmail.com>
2024-01-14 01:36:29 +0530

    feat: Generated stubs from wsdl

---
+++ IPLocationFinder/src/com/example/IPLocationFinder.java
@@ -0,0 +1,17 @@
+package com.example;
+
+public class IPLocationFinder {
+
+	public static void main(String[] args) {
+		// take ip address as arg
+		if(args.length != 1) 
+			System.out.println("Enter IP Address!!!!");
+		else {
+			String ipAddress = args[0];
+			
+			// get service endpoint interface from wsdl
+			// generates stub
+		}
+	}
+
+}
