4c9016ecba263b2698ac9c56cd84fa1185f85526
anushkadeshpande <anushkaa5000@gmail.com>
2024-01-14 01:53:08 +0530

    feat: Added code to invoke a method via stubs

--- IPLocationFinder/src/com/example/IPLocationFinder.java
+++ IPLocationFinder/src/com/example/IPLocationFinder.java
@@ -1,17 +1,28 @@
 package com.example;
 
+import com.lavasoft.GeoIPService;
+import com.lavasoft.GeoIPServiceSoap;
+
 public class IPLocationFinder {
 
 	public static void main(String[] args) {
 		// take ip address as arg
-		if(args.length != 1) 
-			System.out.println("Enter IP Address!!!!");
-		else {
-			String ipAddress = args[0];
-			
+//		if(args.length != 1) 
+//			System.out.println("Enter IP Address!!!!");
+//		else {
+//			String ipAddress = args[0];
+			String ipAddress = "142.251.42.78";
+
 			// get service endpoint interface from wsdl
-			// generates stub
-		}
+			GeoIPService ipService = new GeoIPService();
+			
+			// stub
+			GeoIPServiceSoap ipServiceSoap = ipService.getGeoIPServiceSoap();
+			
+			String ipLocation = ipServiceSoap.getIpLocation(ipAddress);
+			
+			System.out.println(ipLocation);
+//		}
 	}
 
 }
