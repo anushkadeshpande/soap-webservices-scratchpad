e0b38056cf7d722a961f1080974ebdd7768ebe39
anushkadeshpande <anushkaa5000@gmail.com>
2024-01-20 06:46:13 +0530

    fix: Added @WebParam and @WebResult to customize variable names in wsdl

--- TestMart/src/main/java/com/example/ShopInfo.java
+++ TestMart/src/main/java/com/example/ShopInfo.java
@@ -1,6 +1,8 @@
 package com.example;
 
 import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 import javax.jws.soap.SOAPBinding.Style;
@@ -10,8 +12,11 @@ import javax.jws.soap.SOAPBinding.Style;
 @SOAPBinding(style=Style.RPC)
 public class ShopInfo {
 	
+	// @WebParam -- used to change the name of inputVariable in wsdl
+	
 	@WebMethod
-	public String getShopInfo(String property) {
+	@WebResult(partName="lookupOutput")	// to change the name of output variable in wsdl
+	public String getShopInfo(@WebParam(partName="lookupInput") String property) {
 		String response = "Error!!!!!";
 		
 		if("shopName".equals(property)) {
